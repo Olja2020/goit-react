59ea044710b071511aa1c8f0573674d49e45f5be
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-04-24 00:16:04 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
-import userData from "../userData.json";
+// import userData from "../userData.json";
 import Profile from "./profile/Profile";
-import friends from "../friends.json";
+// import friends from "../friends.json";
 import FriendList from "./friendList/FriendList";
 
-export default App() {
+// export default App() {
+//   return (
+//     <>
+//       <Profile
+//         name={userData.username}
+//         tag={userData.tag}
+//         location={userData.location}
+//         image={userData.avatar}
+//         stats={userData.stats}
+//       />
+//       <FriendList friends={friends} />
+//     </>
+//   );
+// };
+import userData from "../userData.json";
+import friends from "../friends.json";
+
+export default function App() {
   return (
     <>
       <Profile
@@ -16,4 +33,4 @@ export default App() {
       <FriendList friends={friends} />
     </>
   );
-};
+}
--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,23 +1,15 @@
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
+//import FriendListItem from "./FriendListItem";
+//import friends from "../friends.json";
 
-const FriendListItem = ({ avatar, name, isOnline }) => (
-  <div>
-    <img src={avatar} alt={name} width="48" />
-    <p>{name}</p>
-    <p>{isOnline}</p>
-  </div>
-);
-FriendListItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.string,
-  avatar: PropTypes.string,
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => {
+        return <li key={friend.id}>{friend}</li>;
+      })}
+    </ul>
+  );
 };
 
-const FriendList = items.map(() => (
-  <ul>
-    <li>
-      <FriendListItem />
-    </li>
-  </ul>
-));
 export default FriendList;
