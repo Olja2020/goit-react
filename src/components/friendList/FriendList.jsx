387319f2624692801a584a937cb7b35f236e0d04
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-04-26 23:28:53 +0300

    add

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,10 +1,10 @@
 //import PropTypes from "prop-types";
 import FriendListItem from "./FriendListItem";
-//import friends from "../friends.json";
+import css from "./FriendList.module.css";
 
 function FriendList({ friends }) {
   return (
-    <ul>
+    <ul className={css.listFriends}>
       {friends.map((friend) => (
         <li key={friend.id}>
           <FriendListItem friend={friend} />
--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types";
-
+import css from "./FriendList.module.css";
+import clsx from "clsx";
 function FriendListItem({ friend: { avatar, name, isOnline } }) {
   return (
-    <div>
+    <div className={css.cardFriend}>
       <img src={avatar} alt={name} width="48" />
-      <p>{name}</p>
-      <p>{isOnline}</p>
+      <p className={css.nameFriend}>{name}</p>
+
+      {isOnline === true ? <p className={clsx(isOnline === true ?
+         css.active : css.offline)}>Online
+
+         </p>
+          : <p className={clsx(isOnline === true ? css.active : css.offline)}>Offline
+            </p>}
     </div>
   );
 }
